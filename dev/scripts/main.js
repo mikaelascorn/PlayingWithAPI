1e70dcd5655e68dcdb289159b8975062e2097dda
Mikaela Scornaienchi <scornaim@gmail.com>
2018-06-15 17:36:00 -0400

    changing API

--- dev/scripts/main.js
+++ dev/scripts/main.js
@@ -1,1 +1,52 @@
-console.log("ready")
+/*
+start with bday info
+Get the product data and use the stored variables to get the data we need.
+
+*/
+const app = {};
+
+app.getBday = (userBday) => {
+  console.log(userBday);
+  
+  // fac3a83aaa654849a854c0e3d010faa9
+
+  let url = `https://api.nytimes.com/svc/search/v2/articlesearch.json`;
+  url += '?' + $.param({
+    'api-key': `d9bea6db046441fd865b5cb6942ff78d`,
+    'q': `technology`,
+    'begin_date': `$(userBday)`,
+    'end_date': `$(userBday)`
+    // 'end_date': "19901123"
+  });
+  $.ajax({
+    url: url,
+    method: 'GET',
+  }).done( (result) => {
+    console.log(result);
+    
+  }).fail( (err) => {
+    throw err;
+  });
+}
+
+app.userInput = () => {
+  $('form').on('submit', function (e){
+    e.preventDefault()        
+
+    const birthday = $('#datepicker').val();
+    console.log(birthday);
+    
+    app.getBday(birthday);
+  });
+};
+
+app.init = () => { // Everything gets called inside of this function    
+  app.userInput();
+};
+
+// Document ready
+$( () => {
+  app.init(); 
+});
+
+
--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,3 +1,52 @@
-"use strict";
+'use strict';
 
-console.log("ready");
+/*
+start with bday info
+Get the product data and use the stored variables to get the data we need.
+
+*/
+var app = {};
+
+app.getBday = function (userBday) {
+  console.log(userBday);
+
+  // fac3a83aaa654849a854c0e3d010faa9
+
+  var url = 'https://api.nytimes.com/svc/search/v2/articlesearch.json';
+  url += '?' + $.param({
+    'api-key': 'd9bea6db046441fd865b5cb6942ff78d',
+    'q': 'technology',
+    'begin_date': '$(userBday)',
+    'end_date': '$(userBday)'
+    // 'end_date': "19901123"
+  });
+  $.ajax({
+    url: url,
+    method: 'GET'
+  }).done(function (result) {
+    console.log(result);
+  }).fail(function (err) {
+    throw err;
+  });
+};
+
+app.userInput = function () {
+  $('form').on('submit', function (e) {
+    e.preventDefault();
+
+    var birthday = $('#datepicker').val();
+    console.log(birthday);
+
+    app.getBday(birthday);
+  });
+};
+
+app.init = function () {
+  // Everything gets called inside of this function    
+  app.userInput();
+};
+
+// Document ready
+$(function () {
+  app.init();
+});
